1e1d7f76cd36aa8e8396d27eaee758c417a24a08
OsipovDN <didison88@gmail.com>
2023-09-01 13:01:50 +0300

    Эксперименты

--- bayan.cpp
+++ bayan.cpp
@@ -1,33 +1,29 @@
 ﻿#include "bayan.h"
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp> 
 #include <iostream>
-#include <boost/program_options.hpp>
+#include <vector>
 
+namespace bf = boost::filesystem;
 namespace po = boost::program_options;
 
-void set_bulk(size_t bulk) {
-    std::cout << "bulk size is " << bulk << std::endl;
-}
+int main(int argc, char* argv[])
+{
+	po::options_description desc;
+	desc.add_options()
+		("help,h", "produce help")
+		("optimization", po::value<std::vector<bf::path>>()->multitoken(), "optimization level");
 
-int main(int argc, const char* argv[]) {
-    try {
-        po::options_description desc{ "Options" };
-        desc.add_options()
-            ("help,h", "This screen")
-            ("config", po::value<std::string>()->default_value("app.yaml"), "config filename")
-            ("bulk", po::value<size_t>()->default_value(5)->notifier(set_bulk), "bulk size");
+	po::variables_map vm;
+	po::store(parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
 
-        po::variables_map vm;
-        po::store(parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);
+	auto temp = vm["optimization"];
 
-        if (vm.count("help"))
-            std::cout << desc << '\n';
-        else if (vm.count("config"))
-            std::cout << "readfrom: " << vm["config"].as<std::string>() << std::endl;
-        else if (vm.count("bulk"))
-            std::cout << "bulk: " << vm["bulk"].as<size_t>() << std::endl;
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
+	if (vm.count("help"))
+		std::cout << desc << '\n';
+	else if (vm.count("optimization"))
+			std::cout << temp.as() << "_____";
+		
+
 }
