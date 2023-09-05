73967389331361205fac53c125dd453a63b5e0c8
OsipovDN <didison88@gmail.com>
2023-09-05 15:50:22 +0300

    Исправлена опция маски пути

--- bayan.cpp
+++ bayan.cpp
@@ -9,10 +9,10 @@
 namespace bf = boost::filesystem;
 namespace po = boost::program_options;
 
-void reg(std::string){}
 
 int main(int argc, char* argv[])
 {
+	boost::regex reg;
 	po::options_description desc("Options");
 	desc.add_options()
 		("help,h", "produce help")
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 		("exclude,e", po::value<std::vector<std::string>>()->multitoken(), "Exclude directories from scanning")
 		("level,l", po::value<int>()->default_value(5), "One for all directories, 0 - only the specified directory without nested")
 		("Min_file_size,s", po::value<long long>()->default_value(1), "By default, all files larger than 1 byte are checked")
-		("file_mask,m", po::value<std::string>()->default_value(".*.")->notifier(reg), "File mask")
+		("file_mask,m", po::value<std::string>()->default_value(".*."), "File mask")
 		("Block_size,S", po::value<int>()->default_value(5), "The size of the block used to read files")
 		("hash_type,t", po::value<std::string>()->default_value("crc32"), "Hashing algorithm type: md5, crc32");
 
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
 	if (vm.count("include")) {
 		std::cout << "include dir: " << std::endl;
 		for (auto it : vm["include"].as<std::vector<std::string>>()) {
-			std::cout<< it << std::endl;
+			std::cout << it << std::endl;
 		}
 	}
 	if (vm.count("exclude")) {
@@ -42,6 +42,9 @@ int main(int argc, char* argv[])
 			std::cout << it << std::endl;
 		}
 	}
+	std::cout << "Mask for path: " << std::endl;
+	std::cout << vm["file_mask"].as<std::string>() << std::endl;
+	reg.assign(vm["file_mask"].as<std::string>());
 
 
 }
