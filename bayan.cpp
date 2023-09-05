24ed3b421d137332b2fa50f81a4852c0ad2fa110
OsipovDN <didison88@gmail.com>
2023-09-05 12:54:22 +0300

    Проработка опций

--- bayan.cpp
+++ bayan.cpp
@@ -12,18 +12,20 @@ int main(int argc, char* argv[])
 	po::options_description desc;
 	desc.add_options()
 		("help,h", "produce help")
-		("optimization", po::value<std::vector<bf::path>>()->multitoken(), "optimization level");
+		("optimization,o", po::value<std::vector<bf::path>>()->composing()->multitoken(), "optimization level");
 
 	po::variables_map vm;
 	po::store(parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
 
-	auto temp = vm["optimization"];
-
 	if (vm.count("help"))
 		std::cout << desc << '\n';
-	else if (vm.count("optimization"))
-			std::cout << temp.as() << "_____";
+	else if (vm.count("optimization")) {
+		
+		for (auto it : vm["optimization"].as<std::vector<bf::path>>()) {
+			std::cout << "readfrom: " << it << std::endl;
+		}
+	}
 		
 
 }
