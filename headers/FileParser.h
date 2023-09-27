d1a2299d1923facb73a41a294cda21c01b2e8c05
OsipovDN <didison88@gmail.com>
2023-09-27 16:48:08 +0300

    Проработка опций в классе FileParser

--- headers/FileParser.h
+++ headers/FileParser.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include <string>
+#include <memory>
 #include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
 
 namespace bf = boost::filesystem;
 
@@ -10,14 +12,12 @@ private:
 	std::vector < bf::path> excluded_dir;
 	int level;
 	long long min_file_size;
-	std::string mask;
-
+	std::unique_ptr<boost::regex >mask;
 
 public:
 	FileParser(std::vector < bf::path> include, std::vector < bf::path> exclude, int l, long long mfs, std::string m) :
-		include_dir(include), excluded_dir(exclude), level(l), min_file_size(mfs), mask(m)
+		include_dir(include), excluded_dir(exclude), level(l), min_file_size(mfs)
 	{
-
-
+		mask=std::make_unique<boost::regex>(m);
 	}
 };
